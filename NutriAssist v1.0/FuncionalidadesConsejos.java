f433f2aba1eff5dbecd0ce4ba7a06d3e4c6e0f94
Elena Rodriguez 21774 <88160455+elenano13@users.noreply.github.com>
2021-11-18 16:36:43 -0600

    Add files via upload

---
+++ NutriAssist v1.0/FuncionalidadesConsejos.java
@@ -0,0 +1,12 @@
+/**ExclusivoClaseS.java 
+*@author Elena Rodríguez 21774 
+*@version 22/08/2021
+*Interfaz que contiene los metodos exclusivos de los consejos 
+*/
+
+public interface FuncionalidadesConsejos{
+	String Beneficios();
+	String Ejemplos();
+	String Cantidad(int peso, int altura, int edad, int sexo);
+	
+}

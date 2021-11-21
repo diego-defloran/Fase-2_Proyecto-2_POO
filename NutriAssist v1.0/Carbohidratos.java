2de3690e936bfd9f83daa8a7743038c79e78848a
Elena Rodriguez 21774 <88160455+elenano13@users.noreply.github.com>
2021-11-20 20:06:10 -0600

    Update Carbohidratos.java

--- NutriAssist v1.0/Carbohidratos.java
+++ NutriAssist v1.0/Carbohidratos.java
@@ -31,14 +31,4 @@ class Carbohidratos extends ConsejosNutricionales{
 	}
 
 	
-	public String Cantidad(int peso, int altura, int edad, int sexo){
-	//Método Cantidad, se utiliza overloading, retorna un string con la cantidad 
-	//Se reciben como parámetros los datos del usuario, se llama al método de la clase madre Calorías y se utiliza para el cálculo en gramos de los carbohidratos
-		mensaje = ("CUANTO DEBO CONSUMIR DE "+nombre);
-		double calorias = Calorias(peso, altura, edad, sexo);
-		double cal = (((calorias*0.133)*100)/200);
-		int total = (int) cal;
-		mensaje = ("\nUsted debe consumir "+total+" gramos carbohidratos al día\n");
-		return mensaje;
-	}
 }
